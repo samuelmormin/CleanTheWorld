d67b0b00c5eb1a729eb25270532be78eae99bdb8
PaulAndrianoff <pandrianoff@gmail.com>
2016-12-14 21:30:44 +0100

    update du main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,8 +16,8 @@ clicker.ressources.gauge_pourcent = document.querySelector(".percentage");
 clicker.ressources.energie_total = document.querySelector(".incrementingEnergie");
 // clicker.ressources.detritus_auto = document.querySelector(".ditritus-auto");
 // clicker.ressources.detritus_auto_value = document.querySelector(".click-detritus-auto-value");
-// clicker.ressources.clicker_level = document.querySelector(".cliker-level");
-console.log(clicker.ressources.planet_images);
+clicker.ressources.shop = document.querySelector('.clicker_shop_block ul');
+
 //Global variables
 clicker.global_var.current_level = 1; // start level
 clicker.global_var.coeficient_price = 2; // coefficient for price
@@ -34,165 +34,190 @@ clicker.global_var.purification_current_percentage = 0
 clicker.global_var.energie_total = 0; //number of energie total
 clicker.global_var.energie_per_sec = 0; //energie per second
 clicker.global_var.purify_per_sec = 0; //purification per second
-clicker.global_var.detritus_per_sec = 10; //detritus per second
-clicker.global_var.click_aids = //list of items to have more detritus per click
-	[{
+clicker.global_var.detritus_per_sec = 0; //detritus per second
+clicker.global_var.next_display_item = 0;
+clicker.global_var.ressources = [
+	{ 
 		name: "gants",
 		value: 2,
 		price: 5,
-		level: 0},
-	 {
-		 name: "pince",
-		 value: 5,
-		 price: 60,
-		 level: 0},
-	 {
-		 name: "sac poubelle",
-		 value: 10,
-		 price: 150,
-		 level: 0},
-	 {
-		 name: "pelle",
-		 value: 30,
-		 price: 320,
-		 level: 0},
-	 {
-		 name: "brouette",
-		 value: 40,
-		 price: 470,
-		 level: 0},
-	 {
-		 name: "pelleteuse",
-		 value: 100,
-		 price: 1000,
-		 level: 0},
-	 {
-		 name: "camion benne",
-		 value: 500,
-		 price: 3000,
-		 level: 0},
-	 {
-		 name: "T 282B",
-		 value: 1000,
-		 price: 7600,
-		 level: 0},
-	];
-
-clicker.global_var.energie_tool = //list of items to get energie auto
-	[{
+		level: 0,
+		type: "click aids"},
+	{
 		name: "banzaï",
 		value_purification: 30,
 		value_energie: 2.5,
 		price: 10,
-		available: 0},
-	 {
-		 name: "arbus",
-		 value_purification: 50,
-		 value_energie: 5,
-		 price: 20,
-		 available: 0},
-	 {
-		 name: "arbre de vie",
-		 value_purification: 70,
-		 value_energie: 10,
-		 price: 80,
-		 available: 0},
-	 {
-		 name: "arbre fruitié",
-		 value_purification: 100,
-		 value_energie: 20,
-		 price: 200,
-		 available: 0},
-	 {
-		 name: "hevea",
-		 value_purification: 300,
-		 value_energie: 40,
-		 price: 900,
-		 available: 0},
-	 {
-		 name: "arbre alienne",
-		 value_purification: 700,
-		 value_energie: 60,
-		 price: 2100,
-		 available: 0},
-	 {
-		 name: "eolienne",
-		 value_purification: 10,
-		 value_energie: 80,
-		 price: 60,
-		 available: 0},
-	 {
-		 name: "barrage",
-		 value_purification: 20,
-		 value_energie: 100,
-		 price: 250,
-		 available: 0},
-	 {
-		 name: "eolienne de l'espace",
-		 value_purification: 30,
-		 value_energie: 160,
-		 price: 780,
-		 available: 0},
-	 {
-		 name: "centrale à fusion",
-		 value_purification: 40,
-		 value_energie: 320,
-		 price: 1300,
-		 available: 0},
-	 {
-		 name: "méga centrale",
-		 value_purification: 50,
-		 value_energie: 780,
-		 price: 2500,
-		 available: 0}];
-clicker.global_var.detritus_auto = //list of items to get detritus auto
-	[{
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "arbus",
+		value_purification: 50,
+		value_energie: 5,
+		price: 20,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "eolienne",
+		value_purification: 10,
+		value_energie: 80,
+		price: 60,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "pince",
+		value: 5,
+		price: 60,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "arbre de vie",
+		value_purification: 70,
+		value_energie: 10,
+		price: 80,
+		available: 0,
+		type: "energie tools"},
+	{
 		name: "bénévole écolo",
 		value: 5,
 		price: 100,
-		available: 0},
-	 {
-		 name: "ouvrier",
-		 value: 10,
-		 price: 210,
-		 available: 0},
-	 {
-		 name: "ouvrier spécialisé",
-		 value: 25,
-		 price: 430,
-		 available: 0},
-	 {
-		 name: "walee",
-		 value: 60,
-		 price: 700,
-		 available: 0},
-	 {
-		 name: "super walee",
-		 value: 100,
-		 price: 1200,
-		 available: 0},
-	 {
-		 name: "régiment alienne",
-		 value: 200,
-		 price: 2200,
-		 available: 0},
-	 {
-		 name: "bataillon intergalactic",
-		 value: 500,
-		 price: 3500,
-		 available: 0},
-	 {
-		 name: "l'armée des écolos intergalactic",
-		 value: 1000,
-		 price: 10000,
-		 available: 0}];
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "sac poubelle",
+		value: 10,
+		price: 150,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "arbre fruitié",
+		value_purification: 100,
+		value_energie: 20,
+		price: 200,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "ouvrier",
+		value: 10,
+		price: 210,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "barrage",
+		value_purification: 20,
+		value_energie: 100,
+		price: 250,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "pelle",
+		value: 30,
+		price: 320,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "ouvrier spécialisé",
+		value: 25,
+		price: 430,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "brouette",
+		value: 40,
+		price: 470,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "walee",
+		value: 60,
+		price: 700,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "panneau solaire",
+		value_purification: 30,
+		value_energie: 160,
+		price: 780,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "hevea",
+		value_purification: 300,
+		value_energie: 40,
+		price: 900,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "pelleteuse",
+		value: 100,
+		price: 1000,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "super walee",
+		value: 100,
+		price: 1200,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "centrale à fusion",
+		value_purification: 40,
+		value_energie: 320,
+		price: 1300,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "arbre alienne",
+		value_purification: 700,
+		value_energie: 60,
+		price: 2100,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "régiment alienne",
+		value: 200,
+		price: 2200,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "méga centrale",
+		value_purification: 50,
+		value_energie: 780,
+		price: 2500,
+		available: 0,
+		type: "energie tools"},
+	{
+		name: "camion benne",
+		value: 500,
+		price: 3000,
+		level: 0,
+		type: "click aids"},
+	{ 																															
+		name: "bataillon intergalactic",
+		value: 500,
+		price: 3500,
+		available: 0,
+		type: "detritus auto"},
+	{
+		name: "T 282B",
+		value: 1000,
+		price: 7600,
+		level: 0,
+		type: "click aids"},
+	{
+		name: "l'armée des écolos intergalactic",
+		value: 1000,
+		price: 10000,
+		available: 0,
+		type: "detritus auto"},
+];
 
-console.log(clicker);
+console.log(clicker.global_var.ressources.length);
 
 // incrementation of detritus
 clicker.ressources.planet_current.addEventListener("click", function(){
 	clicker.global_var.detritus += clicker.global_var.detritus_click_result;
-	clicker.global_var.money = clicker.global_var.money + parseFloat(clicker.global_var.detritus_click_result/10);
+	add_money(clicker.global_var.detritus_click_result, "detritus");
 	clicker.global_var.purification_current = clicker.global_var.purification_current + parseFloat(clicker.global_var.detritus_click_result/2);//10 detritus for 1 purification
 	clicker.ressources.detritus_result.innerHTML = clicker.global_var.detritus;
 	clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
@@ -203,12 +228,11 @@ clicker.ressources.planet_current.addEventListener("click", function(){
 function get_ressources(){
 	clicker.global_var.energie_total += clicker.global_var.energie_per_sec;
 	clicker.ressources.energie_total.innerHTML = clicker.global_var.energie_total;
-
-	clicker.global_var.money = clicker.global_var.money + parseFloat(clicker.global_var.energie_per_sec/5);
+	add_money(clicker.global_var.energie_per_sec, "energie")
 	clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
 
 	clicker.global_var.detritus = parseFloat(clicker.global_var.detritus + clicker.global_var.detritus_per_sec);
-	clicker.global_var.money = clicker.global_var.money + parseFloat(clicker.global_var.detritus_per_sec/10);
+	add_money(clicker.global_var.detritus, "detritus");
 	clicker.global_var.purification_current = clicker.global_var.purification_current + parseFloat(clicker.global_var.detritus_per_sec/2);//10 detritus for 1 purification
 	clicker.ressources.detritus_result.innerHTML = clicker.global_var.detritus;
 
@@ -216,7 +240,18 @@ function get_ressources(){
 	purificationChecker();
 }
 
-var sec_generator = setInterval(get_ressources, 1000);
+//var sec_generator = setInterval(get_ressources, 1000);
+
+function add_money(addCoeff, type){
+	if(type == "detritus"){
+		clicker.global_var.money = clicker.global_var.money + parseFloat(addCoeff/10);
+		console.log(clicker.global_var.money);
+		updateShop();
+	} else if(type == "energie"){
+		clicker.global_var.money = clicker.global_var.money + parseFloat(addCoeff/5);
+		updateShop();
+	}
+}
 
 /*Gauge of purification checker*/
 function purificationChecker(){
@@ -264,3 +299,21 @@ function initialisePlanetImage(){
 	clicker.global_var.current_image = 5;
 	clicker.ressources.planet_images = document.querySelectorAll(".planet div");
 }
+
+function updateShop(){
+	if(clicker.global_var.next_display_item < clicker.global_var.ressources.length){
+		var next_item = clicker.global_var.ressources[clicker.global_var.next_display_item].price;
+		if((next_item == clicker.global_var.money)){
+			var newItem = '<li data-key="'+
+					clicker.global_var.next_display_item + '"> ' +
+					'<img class ="iconStore" src="src/img/gloves.png" alt="ShopItem"><p class="name"> ' +
+					clicker.global_var.ressources[clicker.global_var.next_display_item].name + ' </p> ' +
+					'<p class="price-item"> ' + clicker.global_var.ressources[clicker.global_var.next_display_item].price + 
+					' pièces</p></li>';
+			clicker.ressources.shop.innerHTML += newItem;
+			clicker.global_var.next_display_item++;
+		}
+	}
+}
+
+//clicker.ressources.shop.addEventListener("click", updateShop);
