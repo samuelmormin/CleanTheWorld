ef0dcc41f2125d7a4561cb457b0e218a7d174319
BerangereHetic <berangere.martin@hetic.net>
2016-12-14 21:34:08 +0100

    Modif du script
    Modification du script

--- ajoutDiv.js
+++ ajoutDiv.js
@@ -1,12 +1,7 @@
 var body = document.querySelector('body');
 var clicker_shop_block = document.querySelector('.clicker_shop_block ul');
-var clicker_shop_li = ('.clicker_shop_block ul li');
 
 body.addEventListener('click', function () {
     clicker_shop_block.innerHTML += '<li data-key="37"><img class ="iconStore" src="src/img/gloves.png" alt="ShopItem"><p> Gants </p> <img class="coinIcon" src="src/img/coin50.png" alt="shopCoin"></li>';
 });
 
-clicker_shop_li.addEventListener('click', function () {
-
-});
-console.log();
