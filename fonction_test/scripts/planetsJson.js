56eb77bc0c803fd52c37bc7507aec9bc414314c1
Samuel MORMIN <samuel.mormin@hetic.net>
2016-12-15 18:53:44 +0100

    // Json planets
    - Le fichier Json avec les url des planètes

---
+++ fonction_test/scripts/planetsJson.js
@@ -0,0 +1,5 @@
+{"planets":[
+    {planetName:"planet-", planetImage:[ "src/img/planet1_1.png", "src/img/planet1_2.png", "src/img/planet1_3.png" , "src/img/planet1_4.png", "src/img/planet1_5.png", "src/img/planet1_6.png"]},
+    {planetName:"planet-", planetImage:[ "src/img/planet2_1.png", "src/img/planet2_2.png", "src/img/planet2_3.png" , "src/img/planet2_4.png", "src/img/planet2_5.png", "src/img/planet2_6.png"]},
+    {planetName:"planet-", planetImage:[ "src/img/planet3_1.png", "src/img/planet3_2.png", "src/img/planet3_3.png" , "src/img/planet3_4.png", "src/img/planet3_5.png", "src/img/planet3_6.png"]}
+]}
