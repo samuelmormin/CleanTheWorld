9fa68cf29ff1150e0787541b1464de51e22a4107
PaulAndrianoff <pandrianoff@gmail.com>
2016-12-15 13:24:42 +0100

    main.js update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,6 +28,7 @@ clicker.global_var.detritus = 0; //number of detritus collected
 clicker.global_var.detritus_click_result = 10; //number of detritus collected per click
 clicker.global_var.money_convert_detritus = 10; //detritus convertor rate
 clicker.global_var.money = 0; //Indicator of your money
+clicker.global_var.money_total = 0 //total money collected
 clicker.global_var.purification = 500; // purification level 1
 clicker.global_var.purification_current = 0; //our purification currently
 clicker.global_var.purification_current_percentage = 0
@@ -43,174 +44,201 @@ clicker.global_var.ressources = [
 		value: 2,
 		price: 5,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "banzaï",
 		value_purification: 30,
 		value_energie: 2.5,
 		price: 10,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "arbust",
 		value_purification: 50,
 		value_energie: 5,
 		price: 20,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "eolienne",
 		value_purification: 10,
 		value_energie: 80,
 		price: 60,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "pince",
 		value: 5,
 		price: 65,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "arbre de vie",
 		value_purification: 70,
 		value_energie: 10,
 		price: 80,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "bénévole écolo",
 		value: 5,
 		price: 100,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "sac poubelle",
 		value: 10,
 		price: 150,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "arbre fruitié",
 		value_purification: 100,
 		value_energie: 20,
 		price: 200,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "ouvrier",
 		value: 10,
 		price: 210,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "barrage",
 		value_purification: 20,
 		value_energie: 100,
 		price: 250,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "pelle",
 		value: 30,
 		price: 320,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "ouvrier spécialisé",
 		value: 25,
 		price: 430,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "brouette",
 		value: 40,
 		price: 470,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "walee",
 		value: 60,
 		price: 700,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "panneau solaire",
 		value_purification: 30,
 		value_energie: 160,
 		price: 780,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "hevea",
 		value_purification: 300,
 		value_energie: 40,
 		price: 900,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "pelleteuse",
 		value: 100,
 		price: 1000,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "super walee",
 		value: 100,
 		price: 1200,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "centrale à fusion",
 		value_purification: 40,
 		value_energie: 320,
 		price: 1300,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "arbre alienne",
 		value_purification: 700,
 		value_energie: 60,
 		price: 2100,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "régiment alienne",
 		value: 200,
 		price: 2200,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "méga centrale",
 		value_purification: 50,
 		value_energie: 780,
 		price: 2500,
 		available: 0,
-		typeItem: "energie tools"},
+		typeItem: "energie tools",
+		show: 0},
 	{
 		name: "camion benne",
 		value: 500,
 		price: 3000,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{ 																															
 		name: "bataillon intergalactic",
 		value: 500,
 		price: 3500,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 	{
 		name: "T 282B",
 		value: 1000,
 		price: 7600,
 		level: 0,
-		typeItem: "click aids"},
+		typeItem: "click aids",
+		show: 0},
 	{
 		name: "l'armée des écolos intergalactic",
 		value: 1000,
 		price: 10000,
 		available: 0,
-		typeItem: "detritus auto"},
+		typeItem: "detritus auto",
+		show: 0},
 ];
 
 
@@ -240,14 +268,17 @@ function get_ressources(){
 	purificationChecker();
 }
 
-var sec_generator = setInterval(get_ressources, 1000);
+//var sec_generator = setInterval(get_ressources, 1000);
 
 function add_money(addCoeff, type){
 	if(type == "detritus"){
 		clicker.global_var.money = clicker.global_var.money + parseFloat(addCoeff/10);
+		console.log(parseFloat(addCoeff/10))
+				clicker.global_var.money_total += parseFloat(addCoeff/10);
 		updateShop();
 	} else if(type == "energie"){
 		clicker.global_var.money = clicker.global_var.money + parseFloat(addCoeff/5);
+				clicker.global_var.money_total += parseFloat(addCoeff/10);
 		updateShop();
 	}
 }
@@ -271,28 +302,27 @@ function purificationChecker(){
 	}
 }
 
-function updateLiFonction(){
+function updateLiFonction(thisLi){
+	thisLi--;
 	clicker.ressources.shop_items = document.querySelectorAll('.clicker_shop_block ul li');
-	for(var i = 0; i < clicker.ressources.shop_items.length; i++){
-		clicker.ressources.shop_items[i].addEventListener("click", updateItemShop);
-	}
+	clicker.ressources.shop_items[thisLi].addEventListener("click", updateItemShop);
 }
 
 function planetImagesChange(){
 	if ((clicker.global_var.purification_current_percentage > 17)&&(clicker.global_var.current_image == 5)) {
-		clicker.ressources.planet_images[clicker.global_var.current_image].style = "opacity: 0";
+		clicker.ressources.planet_images[clicker.global_var.current_image].classList.add("planetOpacity");
 		clicker.global_var.current_image--;
 	}else if ((clicker.global_var.purification_current_percentage > 34)&&(clicker.global_var.current_image == 4)) {
-		clicker.ressources.planet_images[clicker.global_var.current_image].style = "opacity: 0";
+		clicker.ressources.planet_images[clicker.global_var.current_image].classList.add("planetOpacity");
 		clicker.global_var.current_image--;
 	}else if ((clicker.global_var.purification_current_percentage > 51)&&(clicker.global_var.current_image == 3)) {
-		clicker.ressources.planet_images[clicker.global_var.current_image].style = "opacity: 0";
+		clicker.ressources.planet_images[clicker.global_var.current_image].classList.add("planetOpacity");
 		clicker.global_var.current_image--;
 	}else if ((clicker.global_var.purification_current_percentage > 68)&&(clicker.global_var.current_image == 2)) {
-		clicker.ressources.planet_images[clicker.global_var.current_image].style = "opacity: 0";
+		clicker.ressources.planet_images[clicker.global_var.current_image].classList.add("planetOpacity");
 		clicker.global_var.current_image--;
 	}else if ((clicker.global_var.purification_current_percentage > 85)&&(clicker.global_var.current_image == 1)) {
-		clicker.ressources.planet_images[clicker.global_var.current_image].style = "opacity: 0";
+		clicker.ressources.planet_images[clicker.global_var.current_image].classList.add("planetOpacity");
 		clicker.global_var.current_image--;
 	}
 }
@@ -300,7 +330,7 @@ function planetImagesChange(){
 function initialisePlanetImage(){
 	var lengthTab = clicker.ressources.planet_images.length;
 	for (var i = 0; i < lengthTab; i++) {
-		clicker.ressources.planet_images[i].style = "opacity:1";
+		clicker.ressources.planet_images[i].classList.remove("planetOpacity");
 	}
 	clicker.global_var.current_image = 5;
 	clicker.ressources.planet_images = document.querySelectorAll(".planet div");
@@ -309,7 +339,8 @@ function initialisePlanetImage(){
 function updateShop(){
 	if(clicker.global_var.next_display_item < clicker.global_var.ressources.length){
 		var next_item = clicker.global_var.ressources[clicker.global_var.next_display_item].price;
-		if((next_item == clicker.global_var.money)){
+		if((next_item <= clicker.global_var.money_total)&&(clicker.global_var.ressources[clicker.global_var.next_display_item].show == 0)){
+			clicker.global_var.ressources[clicker.global_var.next_display_item].show = 1;
 			var newItem = '<li data-key="'+
 					clicker.global_var.next_display_item + '"> ' +
 					'<img class ="iconStore" src="src/img/gloves.png" alt="ShopItem"><p class="name"> ' +
@@ -318,7 +349,7 @@ function updateShop(){
 					' pièces</p></li>';
 			clicker.ressources.shop.innerHTML += newItem;
 			clicker.global_var.next_display_item++;
-			updateLiFonction();
+			updateLiFonction(clicker.global_var.next_display_item);
 		}
 	}
 }
@@ -326,7 +357,7 @@ function updateShop(){
 
 function updateItemShop(){
 	var thisItem = parseInt(this.getAttribute("data-key"));
-//	console.log(clicker.global_var.ressources[thisItem].typeItem);
+	//	console.log(clicker.global_var.ressources[thisItem].typeItem);
 	if(clicker.global_var.ressources[thisItem].typeItem == "click aids") buyDetritusTool(thisItem);
 	else if(clicker.global_var.ressources[thisItem].typeItem == "detritus auto") buyDetritusAids(thisItem);
 	else if(clicker.global_var.ressources[thisItem].typeItem == "energie tools") buyEnergieItem(thisItem);
@@ -339,43 +370,43 @@ function updateItemShop(){
 //buy tool to get more detritus per click
 function buyDetritusTool(item){
 	console.log("buy detritus per click || " + item + clicker.global_var.detritus_click_result);
-		if(clicker.global_var.ressources[item].price <= clicker.global_var.money){
-			clicker.global_var.money -= clicker.global_var.ressources[item].price;
-			clicker.global_var.ressources[item].level++;
-			clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
-			clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * clicker.global_var.coeficient_price);
-			clicker.global_var.detritus_click_result += clicker.global_var.ressources[item].value; 
-//			console.log(clicker.global_var.click_aids[select_value].name + " : " + clicker.global_var.click_aids[select_value].level + " : " + clicker.global_var.ressources[item].price + " pièce");
-			console.log(clicker.ressources.shop_items[item].querySelector(".price-item"));
-			clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
-		}
+	if(clicker.global_var.ressources[item].price <= clicker.global_var.money){
+		clicker.global_var.money -= clicker.global_var.ressources[item].price;
+		clicker.global_var.ressources[item].level++;
+		clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
+		clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * clicker.global_var.coeficient_price);
+		clicker.global_var.detritus_click_result += clicker.global_var.ressources[item].value; 
+		//			console.log(clicker.global_var.click_aids[select_value].name + " : " + clicker.global_var.click_aids[select_value].level + " : " + clicker.global_var.ressources[item].price + " pièce");
+		console.log(clicker.ressources.shop_items[item].querySelector(".price-item"));
+		clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
+	}
 }
 
 //item to have more detritus per sec
 function buyDetritusAids(item){
 	console.log("buy detritus per sec || " + item);
-		if(clicker.global_var.ressources[item].price <= clicker.global_var.money){
-			clicker.global_var.money -= clicker.global_var.ressources[item].price;
-			clicker.global_var.detritus_per_sec += clicker.global_var.ressources[item].value;
-			clicker.global_var.ressources[item].available++;
-			clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * 1.2);
-			clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
-			console.log(clicker.global_var.detritus_per_sec);
-			clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
-		}
+	if(clicker.global_var.ressources[item].price <= clicker.global_var.money){
+		clicker.global_var.money -= clicker.global_var.ressources[item].price;
+		clicker.global_var.detritus_per_sec += clicker.global_var.ressources[item].value;
+		clicker.global_var.ressources[item].available++;
+		clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * 1.2);
+		clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
+		console.log(clicker.global_var.detritus_per_sec);
+		clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
+	}
 }
 
 //item to get energie
 function buyEnergieItem(item){
 	console.log("buy energie per sec || " + item);
-		if(clicker.global_var.ressources[item].price <= clicker.global_var.money){ 
-			clicker.global_var.money -= clicker.global_var.ressources[item].price;
-			clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
-			clicker.global_var.energie_per_sec = parseFloat(clicker.global_var.energie_per_sec + clicker.global_var.ressources[item].value_energie/40);
-			clicker.global_var.purify_per_sec = parseFloat(clicker.global_var.purify_per_sec + clicker.global_var.ressources[item].value_purification/20);
-			clicker.global_var.ressources[item].available++;
-			clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * clicker.global_var.coeficient_price);
-			clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
-		}
+	if(clicker.global_var.ressources[item].price <= clicker.global_var.money){ 
+		clicker.global_var.money -= clicker.global_var.ressources[item].price;
+		clicker.ressources.money.innerHTML = parseInt(clicker.global_var.money);
+		clicker.global_var.energie_per_sec = parseFloat(clicker.global_var.energie_per_sec + clicker.global_var.ressources[item].value_energie/40);
+		clicker.global_var.purify_per_sec = parseFloat(clicker.global_var.purify_per_sec + clicker.global_var.ressources[item].value_purification/20);
+		clicker.global_var.ressources[item].available++;
+		clicker.global_var.ressources[item].price = parseFloat(clicker.global_var.ressources[item].price * clicker.global_var.coeficient_price);
+		clicker.ressources.shop_items[item].querySelector(".price-item").innerHTML = clicker.global_var.ressources[item].price + " pièce";
+	}
 }
 
