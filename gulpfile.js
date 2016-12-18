24d9039a40827043a0043c156ceb872733dc49b2
PaulAndrianoff <pandrianoff@gmail.com>
2016-12-18 11:37:57 +0100

    update debug

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp          = require( 'gulp' ),
 		gulp_concat		= require( 'gulp-concat' ),
 		gulp_uglify		= require( 'gulp-uglify' );
 
-// CSS task
+// CSS style.css
 gulp.task( 'css_style', function(){
 	return gulp.src( './src/styles/style.css')    // Get url of the main CSS file
 		.pipe( gulp_css_nano() )                // Minify it
@@ -13,28 +13,28 @@ gulp.task( 'css_style', function(){
 		.pipe( gulp.dest( './src/styles/' ) );     // Put it in folder if doesn't exist it will creat it
 } );
 
-// CSS task
+// CSS hover
 gulp.task( 'css_hover', function(){
 	return gulp.src( './src/styles/hover.css')    // Get url of the main CSS file
 		.pipe( gulp_css_nano() )                // Minify it
 		.pipe( gulp_rename( 'hover.min.css' ) ) // Rename it
 		.pipe( gulp.dest( './src/styles/' ) );     // Put it in folder if doesn't exist it will creat it
 } );
-// CSS task
+// CSS intro
 gulp.task( 'css_intro', function(){
 	return gulp.src( './src/styles/style_intro.css')    // Get url of the main CSS file
 		.pipe( gulp_css_nano() )                // Minify it
-		.pipe( gulp_rename( 'intro.min.css' ) ) // Rename it
+		.pipe( gulp_rename( 'style_intro.min.css' ) ) // Rename it
 		.pipe( gulp.dest( './src/styles/' ) );     // Put it in folder if doesn't exist it will creat it
 } );
-// CSS task
+// CSS responsive
 gulp.task( 'css_responsive', function(){
 	return gulp.src( './src/styles/responsive.css')    // Get url of the main CSS file
 		.pipe( gulp_css_nano() )                // Minify it
 		.pipe( gulp_rename( 'responsive.min.css' ) ) // Rename it
 		.pipe( gulp.dest( './src/styles/' ) );     // Put it in folder if doesn't exist it will creat it
 } );
-// CSS task
+// CSS div
 gulp.task( 'css_div', function(){
 	return gulp.src( './src/styles/stylesDiv.css')    // Get url of the main CSS file
 		.pipe( gulp_css_nano() )                // Minify it
@@ -58,7 +58,7 @@ gulp.task( 'js_script', function(){
 } );
 // JS task
 gulp.task( 'js_intro', function(){
-	return gulp.src( './src/scripts/script.js')
+	return gulp.src( './src/scripts/intro.js')
 		.pipe( gulp_concat( 'intro.min.js' ) )
 		.pipe( gulp_uglify() )
 		.pipe( gulp.dest( './src/scripts/' ) );
@@ -71,3 +71,5 @@ gulp.task( 'js_ajoutDiv', function(){
 		.pipe( gulp.dest( './src/scripts/' ) );
 } );
 
+
+gulp.task('default', ['css_style', 'css_intro', 'css_responsive', 'css_div', 'js_main', 'js_script', 'js_intro', 'js_ajoutDiv']);
